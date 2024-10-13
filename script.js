57fdda6a584c66d8ed5d78c86ce39d798fea01ee
Rowalewa <lesliewanjala06@gmail.com>
2024-10-13 18:31:54 +0300

    MusicPlayer

---
+++ script.js
@@ -0,0 +1,3 @@
+// for simple arrow functions returning one and just one operation, no need to wrap them in curly brackets and return
+// i.e const function = (num1, num2) => num1 + num2;
+// instead of const function = (num1, num2) => {return num1 + num2};
